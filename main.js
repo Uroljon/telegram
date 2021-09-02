835b88f9e44f4223492ecabd5182567036261d20
Uroljon <uroljonkhidirboev@gmail.com>
2021-09-02 09:47:32 +0500

    left right section

---
+++ main.js
@@ -0,0 +1,34 @@
+let DATA = {
+    users: [
+        {
+            id: 1,
+            first_name: "First Name",
+            last_name: "Last Name",
+            avatar: "avatar1.png",
+            bio: "Bio",
+            user_name: "@username",
+            groups_common: 4,
+            shared_links: 15,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "online",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "lorem ipsum dolor sit amet...",
+                    time: new Date()
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "lorem ipsum dolor sit amet...",
+                    time: new Date()
+                },
+            ]  
+        }
+    ],
+    me: {
+
+    }
+}
