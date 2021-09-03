ff8184394000b1b98f91dbe7dafb4aac4649a9ac
Uroljon <uroljonkhidirboev@gmail.com>
2021-09-03 19:06:35 +0500

    patch

--- main.js
+++ main.js
@@ -1,51 +1,16 @@
-// search function styles
-document.querySelector("#search").addEventListener("focus", ()=>{
-    document.querySelector(".search-wrapper").style.border = "2px solid #3390ec";
-    document.querySelector(".fa-search").style.color = "#3390ec";
-    document.querySelector(".clear-search-btn").style.opacity = "1";
-
-});
-document.querySelector("#search").addEventListener("blur", ()=>{
-    document.querySelector(".search-wrapper").style.border = "1px solid #dfe1e5";
-    document.querySelector(".fa-search").style.color = "#dfe1e5";
-    document.querySelector(".clear-search-btn").style.opacity = "0";
-
-});
-// toggle settings window
-document.querySelector(".toggle-settings").addEventListener("click", ()=>{
-    document.querySelector(".settings").classList.toggle("hide");
-})
-// chat-tabs navigation match
-document.querySelectorAll(".navbar li").forEach((elem)=>{
-    elem.addEventListener("click", (e)=>{
-
-        let location = e.target.getAttribute("data-target");
-
-        // toggle .active on tabs
-        document.querySelectorAll(".navbar li").forEach((elem)=>{
-            elem.classList.remove("active");
-        })
-        e.target.classList.add("active");
-
-        // corresponding tabs will be shown
-        document.querySelectorAll(".tabs-content").forEach((elem)=>{
-            elem.classList.remove("active");
-        });
-        document.querySelector(`#${location}`).classList.add("active");
-    })
-})
-
 let DATA = {
-    users: [
+
+    people: [
         {
             id: 1,
-            first_name: "First Name",
-            last_name: "Last Name",
-            avatar: "avatar1.png",
-            bio: "Bio",
-            user_name: "@username",
+            first_name: "Emma",
+            last_name: "Watson",
+            avatar: "https://api.time.com/wp-content/uploads/2015/04/emma-watson-19.jpg",
+            bio: "Hogwarts",
+            user_name: "@Emma_Watson",
             groups_common: 4,
             shared_links: 15,
+            shared_files: 1,
             shared_photos: 20,
             shared_voice: 15,
             activity: "online",
@@ -53,19 +18,336 @@ let DATA = {
                 {
                     id: 1,
                     is_from_me: false,
+                    text: "Hello",
+                    time: new Date(2021, 7, 16, 11, 11, 11)
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Hi",
+                    time: new Date(2021, 7, 16, 11, 11, 11)
+                },
+            ]
+        },
+        {
+            id: 2,
+            first_name: "Den",
+            last_name: "Radcliffle",
+            avatar: "https://broncolor.swiss/assets/img/Stories/Inspiration/Daniel-Radcliffe/_contentWithShareBar23/DANIEL-RADCLIFFE-featured.jpg",
+            bio: "Hogwarts",
+            user_name: "@Daniel_Radcliffe",
+            groups_common: 5,
+            shared_links: 10,
+            shared_files: 1,
+            shared_photos: 0,
+            shared_voice: 15,
+            activity: "Last seen recently",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "Qalaysan oshna :)",
+                    time: new Date(2021, 7, 19, 11, 11, 11)
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Yaxshi rahmat :)",
+                    time: new Date(2021, 7, 20, 11, 11, 11)
+                },
+            ]
+        },
+        {
+            id: 3,
+            first_name: "Fulfiledu",
+            last_name: "Admin",
+            avatar: "https://static10.tgstat.ru/channels/_0/4b/4bee0aec8083826b79c227eac9d2cf27.jpg",
+            bio: "Fulfil education va Instant company bo'yicha savollaringizni bering!",
+            user_name: "@fulfiledu",
+            groups_common: 1,
+            shared_links: 6,
+            shared_files: 1,
+            shared_photos: 1,
+            shared_voice: 0,
+            activity: "Last seen today at 10:01 AM",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "Assalomu aaykum yaxshimisiz, to'lovingiz tushdi belgilab qo'ydim😊",
+                    time: new Date(2021, 7, 30, 12, 54, 01)
+                },
+
+            ]
+        },
+        {
+            id: 4,
+            first_name: "Asadbek",
+            last_name: "Zoirov",
+            avatar: null,
+            bio: null,
+            user_name: "@ZoirovAsadbek",
+            groups_common: 1,
+            shared_links: 0,
+            shared_files: 1,
+            shared_photos: 0,
+            shared_voice: 0,
+            activity: "Last seen 1 hour ago",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "Assalomu alaykum",
+                    time: new Date(2021, 8, 1, 11, 11, 11)
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Va alaykum assalom. Qalaysiz Asadbek ?",
+                    time: new Date(2021, 8, 2, 11, 11, 11)
+                },
+            ]
+        }
+    ],
+    groups: [
+        {
+            id: 1,
+            name: "MERN va PERN F-13",
+            avatar: null,
+            bio: "Vazifalar uchun: @mernperntasksbot",
+            user_name: "private_group_mern",
+            shared_links: 15,
+            shared_files: 16,
+            shared_photos: 16,
+            shared_voice: 0,
+            shared_videos: 11,
+            activity: "11 members, 2 online",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
                     text: "lorem ipsum dolor sit amet...",
-                    time: new Date()
+                    time: new Date(2021, 8, 1, 11, 11, 11)
                 },
                 {
                     id: 2,
                     is_from_me: true,
                     text: "lorem ipsum dolor sit amet...",
-                    time: new Date()
+                    time: new Date(2021, 8, 1, 11, 11, 50)
                 },
-            ]  
+            ]
+        }
+    ],
+    channels: [
+        {
+            id: 1,
+            name: "Mirzayev's Mind",
+            avatar: "https://static10.tgstat.ru/channels/_0/b0/b06d62818baaf237d626e5360ea562c7.jpg",
+            bio: `
+            ▪️ Entrepreneur
+            ▪️ Investor
+            ▪️ Developer
+            
+            Omad yordir dovyuraklarga! • Судьба благоволит храбрым! • Fates Favors the Brave!
+            Murojaat uchun: @Odilbek_Mirzayev_aloqabot`,
+
+            user_name: "@OdilbekMirzayev",
+            shared_links: 94,
+            shared_files: 1,
+            shared_photos: 72,
+            shared_voice: 3,
+            shared_videos: 9,
+            activity: "1635 subscribers",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "lorem ipsum dolor sit amet...",
+                    time: new Date(2021, 8, 1, 11, 11, 11)
+                },
+                {
+                    id: 2,
+                    is_from_me: false,
+                    text: "lorem ipsum dolor sit amet...",
+                    time: new Date(2021, 8, 2, 11, 11, 11)
+                },
+            ]
+        }
+    ],
+    bots: [
+        {
+            id: 1,
+            name: "VKM Bot",
+            avatar: "https://botostore.com/netcat_files/22/26/preview_26957_1542499501.jpg",
+            bio: `
+            Listen to and download any music! 
+            Contact & Ads: @vkmowner
+            Our media: @vekaem`,
+
+            user_name: "@vkm_bot",
+            shared_links: 22,
+            shared_files: 0,
+            shared_photos: 0,
+            shared_voice: 417,
+            activity: "bot",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "lorem ipsum dolor sit amet...",
+                    time: new Date(2021, 8, 1, 11, 11, 11)
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Mustafa Ceceli",
+                    time: new Date(2021, 8, 1, 11, 11, 11)
+                },
+            ]
         }
     ],
     me: {
 
     }
 }
+let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// search function styles
+document.querySelector("#search").addEventListener("focus", () => {
+    document.querySelector(".search-wrapper").style.border = "2px solid #3390ec";
+    document.querySelector(".fa-search").style.color = "#3390ec";
+    document.querySelector(".clear-search-btn").style.opacity = "1";
+
+});
+document.querySelector("#search").addEventListener("blur", () => {
+    document.querySelector(".search-wrapper").style.border = "1px solid #dfe1e5";
+    document.querySelector(".fa-search").style.color = "#dfe1e5";
+    document.querySelector(".clear-search-btn").style.opacity = "0";
+
+});
+// toggle settings window
+document.querySelector(".toggle-settings").addEventListener("click", () => {
+    document.querySelector(".settings").classList.toggle("hide");
+})
+// chat-tabs navigation match
+document.querySelectorAll(".navbar li").forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+
+        let location = e.target.getAttribute("data-target");
+
+        // toggle .active on tabs
+        document.querySelectorAll(".navbar li").forEach((elem) => {
+            elem.classList.remove("active");
+        })
+        e.target.classList.add("active");
+
+        // corresponding tabs will be shown
+        document.querySelectorAll(".tabs-content").forEach((elem) => {
+            elem.classList.remove("active");
+            elem.innerHTML = ""; // qolgan tab larni ichini tozalaydi
+        });
+        document.querySelector(`#${location}`).classList.add("active");
+        // tanlangan chat ni ichini kontent bilan to'ldiradi
+        fill_left_column(location);
+
+    })
+})
+
+
+function fill_left_column(location) {
+    if (location === "all_chats") {
+        for (let i in DATA) {
+            DATA[i].forEach((item) => {
+                document.querySelector(`#${location}`).innerHTML += `
+            <div class="chat_item" onClick="fill_middle_column(this)" data-username="${item.user_name}">
+                <div class="chat_avatar">
+                    <img src="${item.avatar ? item.avatar : "https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-11.jpg"}" alt="avatar">
+                </div>
+                <div class="user_caption">
+                    <div class="diolog_title">
+                        <p>${item.first_name ? item.first_name : item.name} ${item.last_name ? item.last_name : ""}</p> 
+                        <div class="diolog_title_details">
+                            <i class="fa fa-check message_status" aria-hidden="true"></i>
+                            <span class="message_time">${weekDays[item.messages[item.messages.length - 1].time.getDay()]}</span>
+                        </div>
+                    </div>
+                    <div class="dialog_subtitle">
+                        <p>${item.messages[item.messages.length - 1].text}</p>
+                        <div class="diolog_subtitle_details">
+                            <span class="unread_message_count">${item.messages[item.messages.length - 1].is_from_me ? "" : 1}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                `;
+            });
+
+
+        }
+    } else {//one location for one tab
+        DATA[location].forEach((item) => {
+
+            document.querySelector(`#${location}`).innerHTML += `
+        <div class="chat_item" onClick="fill_middle_column(this)" data-username="${item.user_name}">
+            <div class="chat_avatar">
+                <img src="${item.avatar ? item.avatar : "https://simg.nicepng.com/png/small/930-9302865_notes-from-reasons-to-conference-brighton-whatsapp-profile.png"}" alt="avatar">
+            </div>
+            <div class="user_caption">
+                <div class="diolog_title">
+                    <p>${item.first_name ? item.first_name : item.name} ${item.last_name ? item.last_name : ""}</p> 
+                    <div class="diolog_title_details">
+                        <i class="fa fa-check message_status" aria-hidden="true"></i>
+                        <span class="message_time">${weekDays[item.messages[item.messages.length - 1].time.getDay()]}</span>
+                    </div>
+                </div>
+                <div class="dialog_subtitle">
+                    <p>${item.messages[item.messages.length - 1].text}</p>
+                    <div class="diolog_subtitle_details">
+                        <span class="unread_message_count">${item.messages[item.messages.length - 1].is_from_me ? "" : 1}</span>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+            `;
+        })
+    }
+}
+
+function fill_middle_column(what) {
+
+    console.log(what.getAttribute("data-username"))
+
+    for (let tabs in DATA) {
+        DATA[tabs].forEach((chatItem, index, array) => {
+            if (chatItem.user_name === what.getAttribute("data-username")) {
+                // change avatar
+                document.querySelector("#avatar").src = `${chatItem.avatar ? chatItem.avatar : "https://simg.nicepng.com/png/small/930-9302865_notes-from-reasons-to-conference-brighton-whatsapp-profile.png"}`;
+                // change name
+                document.querySelector("#name").innerHTML = `${chatItem.first_name ? chatItem.first_name : chatItem.name} ${chatItem.last_name ? chatItem.last_name : ""}`;
+                // change status
+                document.querySelector("#status").innerHTML = `${chatItem.activity}`;
+                // re-render new messages after cleaning older ones
+                document.querySelector(".main-chat .container").innerHTML = "";
+                chatItem.messages.forEach((message) => {
+                    document.querySelector(".main-chat .container").innerHTML += `
+                    <div class="message ${message.is_from_me ? "from-me" : "to-me"} has-tail">
+                        ${message.text}
+                    <div class="tail"></div>
+                    </div>
+                    `
+                })
+            }
+        })
+    }
+
+
+}
+
+/***garbage
+ *     // sets event listener for chat items
+    // document.querySelectorAll(".chat-item").forEach((item)=>{
+    //     item.addEventListener("click", fill_middle_column(e));
+    //     console.log("e lis qoshildi")
+    // });//USE ONCLICK(THIS)   instead
+ */
